75a8a5232c07861162721e48cae988df1d625c2b
Alessandro Serra <a.serra132@studenti.unica.it>
2021-04-16 19:38:20 +0200

    updates

--- Esercitazioni/python/fit_non_lineare.py
+++ Esercitazioni/python/fit_non_lineare.py
@@ -7,9 +7,24 @@ def f(x, A, tau, omega):
     return A * np.exp(-x / tau) * np.cos(omega * x)
 
 x = np.linspace(0, 10, 100)
-tau = 5A = 1
-omega =2 * np.pi
+x_new = np.linspace(0, 10, 1000)
+tau = 5
+A = 1
+omega =2 * np.pi / 2
+sigma = .1
 
-y = f(x, A, tau, omega)
+## NOTE: loc indica l'eventuale decentramento della distribuzione normale
+y = f(x, A, tau, omega) + np.random.normal(loc = 0, scale = sigma, size = 100)
 
-plt.sublìplots()
+fig, ax = plt.subplots()
+ax.errorbar(x, y, yerr = sigma * np.ones(100), elinewidth = 1, linewidth = 0, marker = '.', label = 'data')
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+plt.legend()
+
+## NOTE:passiamo alla funzinìone quelli che crediamo essere parametri possibilie gli errori
+
+p, cov = spo.curve_fit(f, x, y,  p0 = [A, tau, omega], sigma = sigma * np.ones(100))
+ax.plot(x_new, f(x_new, p[0], p[1], p[2]), label = 'fit')
+
+plt.show()
