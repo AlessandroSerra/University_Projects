122bd64a21d014b73bac82b479eb7d18714ea38e
Alessandro Serra <a.serra132@studenti.unica.it>
2021-04-17 11:46:55 +0200

    updates

--- Esercitazioni/dinamica molecolare/exp_rutherford.py
+++ Esercitazioni/dinamica molecolare/exp_rutherford.py
@@ -1,5 +1,5 @@
 '''
-programma che riproduce in simulazione l'esperimento di Rutherford sul bombardamento di un foglio d'oro da 
+programma che riproduce in simulazione l'esperimento di Rutherford sul bombardamento di un foglio d'oro da
 parte di particelle alpha. si studia l'angolo di riflessione delle particelle alpha dopo l'eventuale impatto
 con i nuclei d'oro.
 '''
@@ -7,22 +7,22 @@ con i nuclei d'oro.
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.constants as spc
-import vec2d as v                   #importiamo la classe vec2d
+import vec2d as v2d                 #importiamo la classe vec2d
 
 ## NOTE: numero atomico rispettivamente dell'elio e dell'oro
 Ze, Zo = 2, 79
 alpha_mass = 2 * spc.proton_mass + 2 * spc.neutron_mass               #massa di protone e neutrone
 
-## NOTE: forza di cuolomb in forma scalare 
+## NOTE: forza di cuolomb in forma scalare
 Fc = Ze * Zo * spc.e**2 / (4 * np.pi * spc.epsilon_0 * alpha_mass)    #carica elettrone, costante dielettrica nel vuoto
 E = 5e5 * spc.electron_volt                                           #energia di 5MeV convertita in Joule
-dis = F0 / E * alpha_mass                                              #distanza minima tra particella sparata e nucleo 
+dis = Fc / E * alpha_mass                                              #distanza minima tra particella sparata e nucleo
 vel = np.sqrt(2 * E / alpha_mass)
 tau = dis / vel
 
 def F(r):
     mod = r.mod()
-    return F0 / mod**3 * r      #F(r) restituisce la forza di coulomb vettoriale
+    return Fc / mod**3 * r      #F(r) restituisce la forza di coulomb vettoriale
 
 def step(r, v, tau):                            #velocity-verlet
     rnew = r + v * tau + F(r) * tau**2 / 2
@@ -38,10 +38,34 @@ def solve(r0, v0, tau, Nsteps):
         t.append(t[i] + tau)
         r.append(rnew)
         v.append(vnew)
-        
+
     return t, r, v
 
-r0 = v.vec2d(-100 * dis, 100 *(2 * np.random.rand() - 1) * dis) #rand() restituisce un numero tra 0 ed 1 ma noi lo vogliamo tra -100 e 100 d
-v0 = v.vec2d(vel, 0)        #all'inizio vogliamo moto solo lungo x
+theta = []
+Nparticles = 1000
 
-Nsteps = 2 * int(r0  / dis)
+for i in range(Nparticles):
+    r0 = v2d.vec2d(-100 * dis, 100 *(2 * np.random.rand() - 1) * dis) #rand() restituisce un numero tra 0 ed 1 ma noi lo vogliamo tra -100 e 100 d
+    v0 = v2d.vec2d(vel, 0)        #all'inizio vogliamo moto solo lungo x
+
+    Nsteps = 2 * int(r0.mod()  / dis)
+
+    t, r, v = solve(r0, v0, tau, Nsteps)
+    theta.append(v0.get_angle(v[-1]))
+
+## NOTE: vogliamo vedere come sono distribuiti gli angoli theta delle N particelle alpha
+fig, ax = plt.subplots()
+ax.hist(theta, histtype = 'step')       #creiamo un istogramma
+ax.set_yscale('log')                    #usiamo scala log per vedere più suddivisioni
+ax.set_xlabel('$\\theta$')
+ax.set_ylabel('counts')
+
+'''
+fig, ax = plt.subplots()
+ax.plot([pos.x / dis for pos in r], [pos.y / dis for pos in r])
+ax.plot(0, 0, marker = '.', color = 'k')        #disegnamo l'atomo d'oro nell'origine
+ax.set_xlabel('x (d)')
+ax.set_ylabel('y (d)')
+'''
+#plt.legend()
+plt.show()
---
+++ Esercitazioni/dinamica molecolare/vec2d.py
@@ -0,0 +1,59 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+## NOTE: definiamo la classe vettori in due dimensioni, la funzione _init_ serve a dare dei valori ad x ed
+class vec2d:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    ## NOTE: possiamo fornire infiniti attributi ad una classe definendo delle funziono
+    def mod(self):
+        return np.sqrt(self.x ** 2 + self.y ** 2)
+
+    def unitary(self):
+        modulo = self.mod()
+        return vec2d(self.x / modulo, self.y / modulo)
+
+    def __add__(self, other):
+        return vec2d(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other):
+        return vec2d(self.x - other.x, self.y - other.y)
+
+    def __mul__(self, other):
+
+        if type(other) == vec2d:
+            return self.x * other.x, self.y * other.y
+
+        return vec2d(self.x * other, self.y * other)
+
+    def __rmul__(self, other):
+        return self * other
+
+    def __truediv__(self, other):
+        return self * (1 / other)
+
+    def __str__(self):
+        return '(' + str(self.x) + ',' + str(self.y) + ')'
+
+    def get_angle(self, other):
+        return np.arccos(self * other / (self.mod() * other.mod()))
+
+
+'''
+v = vec2d(3, 4)
+print(v.x)
+print(v.y)
+print(v.mod())
+u = v.unitary()
+print(u.x, u.y)
+print(u.mod())          #test vari
+print(u+v)
+print(u-v)
+print(v*2)
+print(v.mod() * 2)
+print(v*u)
+print(v/2)
+'''
