81c4f9fd6b3b66338e9e93c4c2050f1241522700
AlessandroSerra <a.serra132@studenti.unica.it>
2021-04-25 23:29:30 +0200

    updates

--- Esercizi/I_PARZIALE/prova.py
+++ Esercizi/I_PARZIALE/prova.py
@@ -1,9 +1,13 @@
 import numpy as np
 import vec2d as v
 
-N_particles = 2
-N_steps = 1
+N_particles = 3
+N_steps = 2
+a = np.zeros((N_particles, N_steps))
+b = [1, 2, 3]
 
-int[][] matrix = int[4][5]
+for i in range(N_steps):
+    for l in range(N_particles):
+        a[l, i] = b[l]
 
-print(pos.shape)
+print(a)
