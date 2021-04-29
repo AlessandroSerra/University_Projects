fef0d41006d0e6c4e656c5d647b08dac08d613e4
AlessandroSerra <a.serra132@studenti.unica.it>
2021-04-29 18:13:58 +0200

    updates

--- Esercizi/I_PARZIALE/rutherford/rutherford_exp_3D.py
+++ Esercizi/I_PARZIALE/rutherford/rutherford_exp_3D.py
@@ -13,22 +13,24 @@ def init_values(N_particles, N_steps, Ze, Zo, energy, alpha_mass):
     pos_array = np.full((N_particles, N_steps), fill_value = v3d.vec3d(0, 0, 0))
     vel_array = np.full((N_particles, N_steps), fill_value = v3d.vec3d(0, 0, 0))
     impact_parameter = Ze * Zo * spc.e**2 / (4 * np.pi * spc.epsilon_0 * alpha_mass) * alpha_mass / energy
-    theta_list = []
+    angles_list = []
 
+    emittent_radius = 5
     pos_x = -100 * impact_parameter
     vel_x = np.sqrt(2 * energy / alpha_mass)
 
+
     for k in range(N_particles):
         
-        pos_y = 100 * (2 * np.random.rand() - 1) * impact_parameter
-        pos_z = 100 * (2 * np.random.rand() - 1) * impact_parameter
+        pos_y = emittent_radius * (2 * np.random.rand() - 1) * impact_parameter
+        pos_z = emittent_radius * (2 * np.random.rand() - 1) * impact_parameter
         pos_array[k, 0] = v3d.vec3d(pos_x, pos_y, pos_z)
         vel_array[k, 0] = v3d.vec3d(vel_x, 0, 0)
 
-    return pos_array, vel_array, theta_list, impact_parameter
+    return pos_array, vel_array, angles_list, impact_parameter
 
 
-def acc_coulomb(pos_array, Ze, Zo, alpha_mass, i):
+def get_acceleration(pos_array, Ze, Zo, alpha_mass, i):
 
     acc_list = []
 
@@ -61,13 +63,13 @@ def update_vel(vel_array, acc, acc_new, N_particles, tau, i):
     return vel_temp_list
 
 
-#def scattering_angles_curve_fitting(theta_list, N_particles):
+def scattering_angles_curve_fit(angles_list, N_particles):
 
-    def theor_distribution(theta_list, N_particles, alpha):
+    def theor_distribution(angles_list, N_particles, alpha):
 
-        return N_particles / (2 * np.sin(theta_list / 2)**alpha)
+        return N_particles / np.power(np.sin(angles_list), alpha)
 
-    counts, bins = np.histogram(theta_list, bins = 30)   
+    counts, bins = np.histogram(angles_list, bins = 30)   
     bins = bins[1:] - (bins[1] - bins[0]) / 2
     p, cov = spo.curve_fit(theor_distribution, bins, counts, p0 = [1, 2], sigma = counts)
     x  = np.linspace(bins[0], bins[-1], 1000)
@@ -78,40 +80,35 @@ def update_vel(vel_array, acc, acc_new, N_particles, tau, i):
 
 def run_rutherford_exp3D(N_particles, N_steps, Ze, Zo, energy, alpha_mass, tau):
 
-    pos_array, vel_array, theta_list, impact_parameter = init_values(N_particles, N_steps, Ze, Zo, energy, alpha_mass)
-    acc = acc_coulomb(pos_array, Ze, Zo, alpha_mass, 0)
+    pos_array, vel_array, angles_list, impact_parameter = init_values(N_particles, N_steps, Ze, Zo, energy, alpha_mass)
+    acc = get_acceleration(pos_array, Ze, Zo, alpha_mass, 0)
 
     for i in range(N_steps - 1):
 
         pos_array[:, i + 1] = update_pos(pos_array, vel_array, acc, N_particles, tau, i)
-        acc_new = acc_coulomb(pos_array, Ze, Zo, alpha_mass, i + 1)
+        acc_new = get_acceleration(pos_array, Ze, Zo, alpha_mass, i + 1)
         vel_array[:, i + 1] = update_vel(vel_array, acc, acc_new, N_particles, tau, i)
-
         acc = acc_new
 
     for i in range(N_particles):
 
-        theta_list.append(vel_array[i, 0].get_angle(vel_array[i, -1], 'rad'))
+        angles_list.append(vel_array[i, 0].get_angle(vel_array[i, -1], 'rad'))
 
-    return pos_array, vel_array, theta_list, impact_parameter
+    return pos_array, vel_array, angles_list, impact_parameter
 
-
-## NOTE: main 
 
 ## NOTE: parametri di simulazione
-N_particles = 1000
+N_particles = 10
 N_steps = 200
-energy = 5e5 * spc.electron_volt                        #energia di 5MeV convertita in Joule
-tau = 9.3e-20
+energy = 5e5 * spc.electron_volt    #energia di 5MeV convertita in Joule
+tau = 9.3e-20                       #secondi
+Ze, Zo = 2, 79                                              #numero atomico di elio (Ze) ed oro (Zo)
+alpha_mass = 2 * spc.proton_mass + 2 * spc.neutron_mass     #massa atomica elio in uma
 
-## NOTE: numero atomico rispettivamente dell'elio e dell'oro
-Ze, Zo = 2, 79
-alpha_mass = 2 * spc.proton_mass + 2 * spc.neutron_mass 
+what_to_do = input('Insert "t" to see the alpha particles trajectories or "a" to see the scattering angles\t')
 
-what_to_do = input('Inserire "t" per le traiettorie e "a" per gli angoli\t')
-
-pos_array, vel_array, theta_list, impact_parameter = run_rutherford_exp3D(N_particles, N_steps, Ze, Zo, energy, alpha_mass, tau)
-#x_fit, y_fit = scattering_angles_curve_fitting(theta_list, N_particles)
+pos_array, vel_array, angles_list, impact_parameter = run_rutherford_exp3D(N_particles, N_steps, Ze, Zo, energy, alpha_mass, tau)
+x_fit, y_fit = scattering_angles_curve_fit(angles_list, N_particles)
 
 ## NOTE: parte del codice dedicata esclusivamente alla rappresentazione grafica
 
@@ -119,32 +116,32 @@ if what_to_do == 't':
 
     fig = plt.subplots()
     ax = plt.axes(projection = '3d')
-    ax.plot3D(0, 0, 0, marker = 'o', color = 'r')
+    ax.plot3D(0, 0, 0, marker = 'o', color = '#FFD700')
 
     for i in range(N_steps):
         for k in range(N_particles):
 
             ax.plot3D([pos_array[k, i].x / impact_parameter],[pos_array[k, i].y / impact_parameter], [pos_array[k, i].z / impact_parameter], marker = '.', markersize = .5, color = 'b')
 
-    ax.set_title('Esperimento di Rutherford in 3 dimensioni')
-    ax.set_xlabel('x (d)')
-    ax.set_ylabel('y (d)')
-    ax.set_zlabel('z (d)')
+    ax.set_title('3D Rutherford Experiment')
+    ax.set_xlabel('x / impact parameter')
+    ax.set_ylabel('y / impact parameter')
+    ax.set_zlabel('z / impact parameter')
 
 elif what_to_do == 'a': 
 
         fig, ax = plt.subplots()
-        ax.hist(theta_list, histtype = 'step', bins = 30, label = 'Data')
+        ax.hist(angles_list, histtype = 'step', bins = 30, label = 'Data')
         ax.set_yscale('log')
         ax.set_xlabel('$\\theta$')
         ax.set_ylabel('Counts')
-        ax.set_title('Istogramma della distribuzione degli angoli di scattering')
-        #ax.plot(x_fit, y_fit, label = 'fit angoli di scattering', linewidth = 1)
+        ax.set_title('Distribution of the Angles Scattering in 3 dimentions')
+        ax.plot(x_fit, y_fit, label = 'Scattering angles fit', linewidth = 1)
 
 else:
-    print('wrong code, try again')
+    print('Wrong code, use "t" to see the trajectories or "a" to see the angles distribution\n')
 
-print('Tempo impiegato dalla simulazione:', time.time() - t_s, 'secondi\n')
+print('Time taken by the simulation:\t', time.time() - t_s, 'seconds\n')
 
 plt.legend()
 plt.show()
